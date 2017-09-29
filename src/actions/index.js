2ee5cbeeb2770b0b2968f3e8541c6bc42a8a1251
tiagogomes-bd <tiagogomes@boldcom.pt>
2017-09-29 13:13:00 +0100

    change the app to use react-redux instead of redux only

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
 export const ADD_RECIPE           = 'ADD_RECIPE'
 export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
 
-export function addRecipe({ day, recipe, meal }) {
+export function addRecipe({ day, meal, recipe }) {
   return {
     type:ADD_RECIPE,
     day,
-    recipe,
-    meal
+    meal,
+    recipe
   }
 }
 
